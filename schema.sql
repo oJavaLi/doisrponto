96e52178fdf9fefd321d762ace96ca880b7369e8
Ana Damasceno <107864553+anadamascenos@users.noreply.github.com>
2023-09-12 20:41:41 -0300

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,4 +1,4 @@
-CREATE TABLE usuario (
+CREATE TABLE usuarios (
     matricula INT PRIMARY KEY,
     nome VARCHAR(255),
     senha VARCHAR(255),
@@ -11,13 +11,13 @@ CREATE TABLE centro_de_resultados (
     nome_cliente VARCHAR(255)
 );
 
-CREATE TABLE status_apontamento (
+CREATE TABLE status_apontamentos (
     apontamento_id INT,
-    avaliador_matricula INT,
+    avaliador_matricula INT,-----
     aprovado BOOLEAN,
     justificativa TEXT,
     FOREIGN KEY (apontamento_id) REFERENCES apontamentos(id),
-    FOREIGN KEY (avaliador_matricula) REFERENCES usuario(matricula)
+    FOREIGN KEY (avaliador_matricula) REFERENCES usuario(matricula)------
 );
 
 CREATE TABLE apontamentos (
@@ -26,8 +26,8 @@ CREATE TABLE apontamentos (
     data_hora_fim TIMESTAMP,
     id SERIAL PRIMARY KEY,
     justificativa TEXT,
-   usuario_matricula INT,
-   centro_resultados_id INT,
+    usuario_matricula INT,
+    centro_resultados_id INT,
     FOREIGN KEY (usuario_matricula) REFERENCES usuario(matricula),
     FOREIGN KEY (centro_resultados_id) REFERENCES centro_de_resultados(id)
 );
