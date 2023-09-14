6581d2bb77167fa455f3a0a3a290e4674158bc6a
Alessandra Moreira <alessandra.moreira01@fatec.sp.gov.br>
2023-09-13 21:50:55 -0300

    inserindo a dependência do Data JPA

--- doisrponto/src/main/java/com/ojavali/doisrponto/UserRepository.java
+++ doisrponto/src/main/java/com/ojavali/doisrponto/UserRepository.java
@@ -1,9 +1,10 @@
 package com.ojavali.doisrponto;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository <User, Long> {
 
    /* coloca aqui o crud */
 }
