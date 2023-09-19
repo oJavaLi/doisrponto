c08c8d20cb5c508aff507f6fa7b19867de75024c
Jhonny Freitas Dutra <dutrajy@gmail.com>
2023-09-18 21:16:16 -0300

    Removendo spring security e adicionando menssagem padrão da API

--- src/main/java/com/ojavali/doisrponto/DoisrpontoApplication.java
+++ src/main/java/com/ojavali/doisrponto/DoisrpontoApplication.java
@@ -1,13 +1,27 @@
 package com.ojavali.doisrponto;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class DoisrpontoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DoisrpontoApplication.class, args);
 	}
 
+	@GetMapping("/")
+	public ResponseEntity<Object> index() {
+		Map<String, String> data = new HashMap<>();
+		data.put("message", "Bem vindo a API 2RPonto.");
+		return new ResponseEntity<>(data, HttpStatus.OK);
+	}
 }
