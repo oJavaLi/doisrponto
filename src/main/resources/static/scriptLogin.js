15659b8816ed3ab8f842bf52723e34b903e32dc4
Alessandra Moreira <alessandra.moreira01@fatec.sp.gov.br>
2023-09-24 14:33:00 -0300

    conexao login com banco

---
+++ src/main/resources/static/scriptLogin.js
@@ -0,0 +1,39 @@
+const loginForm = document.querySelector("form");
+
+loginForm.addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+
+
+    // Construa o objeto de dados a ser enviado
+    const loginData = {
+        matricula: username,
+        senha: password
+    };
+
+    console.log(loginData)
+
+    // Faça uma solicitação POST para o endpoint de autenticação no servidor Spring Boot
+    fetch("http://localhost:1234/login", {
+        method: "POST",
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(loginData)
+    })
+        .then(response => {
+            if (response.status === 200) {
+                // Autenticação bem-sucedida, você pode redirecionar o usuário para outra página ou executar ações apropriadas.
+                console.log("Autenticação bem-sucedida");
+            } else {
+                // Autenticação falhou, você pode exibir uma mensagem de erro ao usuário.
+                console.error("Falha na autenticação");
+            }
+        })
+        .catch(error => {
+            console.error("Erro na solicitação:", error);
+        });
+});
